89fd0402f0716261010a03c0d51e19d2cd9c31e7
Oleksandr <sorokin.biochemistry@gmail.com>
2020-11-16 00:52:51 +0100

    First MySql Client backbone

---
+++ DatabaseClient/Subscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseClient
+{
+    public class Subscription
+    {
+        public string UserID { get; set; }
+        public string Location { get; set; }
+        public int RequestsPerHour { get; set; }
+        public string SubscriptionKey { get; set; }
+        public bool Active { get; set; }
+        public string Status { get; set; }
+    }
+}

b48fe23bf797eeb2ffcf6fe23097d1d26c457e48
Oleksandr <sorokin.biochemistry@gmail.com>
2020-11-17 20:11:54 +0100

    added mySqlClient

--- DatabaseClient/MySqlDatabaseClient.cs
+++ DatabaseClient/MySqlDatabaseClient.cs
@@ -1,8 +1,47 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Repository;
+using System.Linq;
 
 namespace DatabaseClient
 {
-    class MySqlDatabaseClient : IRepository<>
+    class MySqlDatabaseClient : DbContext, IRepository<Subscription>
     {
+        public DbSet<Subscription> subscription { get; set; }
+        DbContextOptionsBuilder optionsBuilder;
+
+        public MySqlDatabaseClient(string server, string library, string user, string password)
+        {
+            optionsBuilder.UseMySQL($"server={server};database={library};user={user};password={password}");
+        }
+
+        public void Create(Subscription subscribe)
+        {
+            Database.EnsureCreated();
+            subscription.Add(subscribe);
+        }
+
+        public void Delete(string id)
+        {
+            var sub = new Subscription { ID = id };
+            subscription.Remove(sub);
+        }
+
+        public Subscription Read(string id)
+        {
+            return subscription.FirstOrDefault(c => c.ID == id);
+        }
+
+        public void Update(string id, string lastSent)
+        {
+            Subscription read = Read(id);
+            read.LastSent = int.Parse(lastSent);
+            Delete(id);
+            Create(read);
+        }
+
+        public void Save()
+        {
+            SaveChanges();
+        }
     }
 }
--- DatabaseClient/Subscription.cs
+++ DatabaseClient/Subscription.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DatabaseClient
+﻿namespace DatabaseClient
 {
     public class Subscription
     {
+        public string ID { get; set; }
         public string UserID { get; set; }
         public string Location { get; set; }
         public int RequestsPerHour { get; set; }
-        public string SubscriptionKey { get; set; }
         public bool Active { get; set; }
         public string Status { get; set; }
+        public int ExpiredAt { get; set; }
+        public int LastSent { get; set; }
     }
 }
